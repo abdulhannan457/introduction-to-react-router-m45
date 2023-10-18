373be593a86457220380617f1806831b4062a7de
abdulhannan <abdulhannan24434457@gmail.com>
2023-10-18 11:47:13 +0600

    added Nav Link in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
@@ -9,10 +9,13 @@ const Header = () => {
                 <span>My website</span><br></br>
                <Link to={'/'}>Home</Link>
                <Link to={'/about'}>About</Link>
-               <Link to={'/users'}>Users</Link>
+               <NavLink to={'/users'}>users</NavLink>
+              
                <Link to={'/contact'}>Contact</Link>
-               <Link to={'/posts'}>posts</Link>
-               <Link to={'/comments'}>Comments</Link>
+               <NavLink to={'/posts'}>Posts</NavLink>
+            
+               
+               <NavLink to={'/comments'}>Comments</NavLink>
                <Link to={'/todos'}>Todos</Link>
             </nav>
         </div>
